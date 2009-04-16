e695d56c19d536edd29c1219d816d049aee025dc
Roman Bogorodskiy <bogorodskiy@gmail.com>
2009-04-16 21:50:33 +0400

    Provide setup.py for easy installation.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+        name = "gg",
+        version = "0.1",
+        author = "Roman Bogorodskiy",
+        author_email = "rbogorodskiy@griddynamics.com",
+        url="http://github.com/novel/gg/tree",
+        py_modules = ["GoGridClient", "GoGridManager"],
+        scripts = ["gg-image-list", "gg-ip", "gg-lookup", 
+            "gg-password", "gg-raw", "gg-server-add", 
+            "gg-server-delete", "gg-server-delete-all", 
+            "gg-server-list", "gg-server-power"]
+)
