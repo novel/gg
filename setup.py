fbb108b50992e82c95c0654cd39dbecd929c5bef
Roman Bogorodskiy <bogorodskiy@gmail.com>
2009-08-13 17:06:09 +0400

    Prepare release 0.2.0.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         author_email = "rbogorodskiy@griddynamics.com",
         url="http://github.com/novel/gg/tree",
         py_modules = ["GoGridClient", "GoGridManager"],
-        scripts = ["gg-image-list", "gg-ip", "gg-lookup", 
+        scripts = ["gg-image-list", "gg-image-save", "gg-ip", "gg-lookup", 
             "gg-password", "gg-raw", "gg-server-add", 
             "gg-server-delete", "gg-server-delete-all", 
             "gg-server-list", "gg-server-power"]
